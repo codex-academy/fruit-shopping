b5e505577c2c15d5b94e75a466a9a3efba783c85
avermeulen <andre.vermeulen@gmail.com>
2017-05-27 09:03:07 +0200

    cleanup screen text

--- test/fruitshop.logic.tests.js
+++ test/fruitshop.logic.tests.js
@@ -1,13 +1,14 @@
-    describe('The Fruit shop', function(){
+    describe('The Fruitshop', function(){
 
-        function extractUserScreenSelection(){
-            return {
-                language : document.querySelector("input[name='language']:checked"),
-                firstName : document.querySelector(".firstName")
-            }
-        }
+        it('should be able to sell 3 apples for R7.50', function(){
+            assert.equal(1,2);
+        });
 
-        it('should be able to calculate the price for the fruit selected', function(){
+        it('should be able to sell 2 bananas for R3.50', function(){
+            assert.equal(1,2);
+        });
+
+        it('should be able to sell 5 apples for R7.50', function(){
             assert.equal(1,2);
         });
 
